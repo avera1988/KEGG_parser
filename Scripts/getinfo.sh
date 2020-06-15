fb7a4653ac85fd0895ab2fb0bbaa2d9460efd659
avera1988 <36417540+avera1988@users.noreply.github.com>
2020-06-15 14:03:18 -0400

    Add files via upload

---
+++ Scripts/getinfo.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+#Script to obtain KEGG annotations and Pathways#
+
+
+scripts='/home/avera/scripts/KEGG.analysis.dir'
+cp $scripts/*.R .
+	
+for i in *ko.txt;
+	do
+	cat $i|awk '{if($2 ~ /^K/) print $2}' > $i.to.keggrest;
+	cat $i|awk '{if($2 ~ /^K/) print }' > $i.mod;
+done
+#Apply getInfo.R script
+
+for i in *.keggrest;
+	do
+	Rscript getInfo.warp.R $i $i.annot
+done
+
+#Obtain Pathway
+
+for i in *.mod
+	do
+	name=$(basename $i .mod)
+	Rscript get.PathwayFromKegg.R $i $name.to.keggrest.annot $name.Pathway
+done
+
+rm *.R
