bd9ce6c919150a9f4ed878a84c977219364e544e
avera1988 <36417540+avera1988@users.noreply.github.com>
2020-06-16 11:25:41 -0400

    Update getInfoFromKegg.R

--- Scripts/getInfoFromKegg.R
+++ Scripts/getInfoFromKegg.R
@@ -1,33 +1,17 @@
 ###########################################
 # Program name: getInfoFromKegg.r
 # Author: Leticia Vega Alvarado
-# Last modification: 20/ago/2015
-# Description: Este programa se encarga de hacer búsquedas en la base de datos de KEGG, en particular
-#              obtiene el campo DEFINITION.
-# Parametros: getInfoFromKegg<-function(fnInputFileName,fnOutputFileName,fnField)
-#            fnInputFileName: archivo con los identificadores (K00820), un identificador por linea. El archivo debe contener el path de ubicacion
-#            fnOutputFileName: nombre del archivo de donde se colocaran los resultados. El nombre debe incluir el path de ubicacion del archivo
-# Dependencias: Bioconductor, KEGGREST
-# Example: In R
-#          source("/home/lety/bin/getInfoFromKegg.r")
-#          getInfoFromKegg("/home/lety/bin/prueba.txt","/home/lety/bin/prueba_KeggRes.txt")
-#
-#          El archivo prueba.txt contiene:
-#          K00820
-#          K04527
-#          K00106
-#
-#          El archivo prueba_KeggRes.txt contiene:
-#          names	DEFINITION
-#          K00820	glucosamine--fructose-6-phosphate aminotransferase (isomerizing) [EC:2.6.1.16]
-#          K04527	insulin receptor [EC:2.7.10.1]
-#          K00106	xanthine dehydrogenase/oxidase [EC:1.17.1.4 1.17.3.2]
-#
+# Created: 09/20/2015
+# Last modification: 06/15/2020 by Arturo Vera
+# This software annotate a series of KO values wiht the Description in KEGG database
 ###########################################
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install("KEGGREST")
+if(!require("KEGGREST")){
+	BiocManager::install("KEGGREST")
+	library(KEGGREST)
+} 
 
 getInfoFromKegg<-function(fnInputFileName,fnOutputFileName)
 {
