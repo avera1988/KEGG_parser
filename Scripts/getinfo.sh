82600b4912c7691046ecd144b5eb95123a77f822
avera1988 <36417540+avera1988@users.noreply.github.com>
2020-06-16 12:42:35 -0400

    Update getinfo.sh

--- Scripts/getinfo.sh
+++ Scripts/getinfo.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/bash
-
+#######################################################################
 #Script to obtain KEGG annotations and Pathways#
 #"Usage:bash getinfo.sh path_to_KEGG_parser_Scripts"
+#Author Aruro Vera
+# 06/15/2020
+#######################################################################
 
 print_usage(){
 	echo "Usage: $0 path_to_KEGG_pasrser_scripts"
